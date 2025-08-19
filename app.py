ce15aea6b98298ad9559c0975deaa5f2eba18fee
Mindarina23 <154536870+Mindarina23@users.noreply.github.com>
2025-08-19 16:45:26 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from ultralytics import YOLO
 import streamlit as st
+from ultralytics import YOLO
 from PIL import Image
+import numpy as np
 
-model = YOLO("best.pt")  # pastikan file ini ada di repo
+st.title("Demo YOLOv11 - Deteksi Penyakit Daun Padi")
 
-st.title("YOLOv11 Rice Disease Detection Demo")
+@st.cache_resource
+def load_model():
+    return YOLO("best.pt")   # pastikan file best.pt ada
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg","jpeg","png"])
+model = load_model()
+
+uploaded_file = st.file_uploader("Upload gambar daun padi", type=["jpg","jpeg","png"])
+
 if uploaded_file is not None:
-    img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image")
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Gambar asli", use_container_width=True)
 
-    results = model.predict(img)
-    res_plotted = results[0].plot()
-    st.image(res_plotted, caption="Prediction Result")
+    st.write("🔍 Sedang mendeteksi...")
+    results = model.predict(np.array(img), conf=0.5, verbose=False)
+
+    for r in results:
+        vis_bgr = r.plot()
+        vis_rgb = vis_bgr[:, :, ::-1]
+        st.image(vis_rgb, caption="Hasil deteksi", use_container_width=True)
